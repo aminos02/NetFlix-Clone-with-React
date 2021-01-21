241cb783385a9ec0ab2d17111f8cf905da133da5
aminos02 <azzouzamine02@gmail.com>
2021-01-21 06:10:18 +0100

    End Project

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Row from "./Row";
 import "./App.css";
 import requests from "./request";
 import Banner from "./Banner";
+import Nav from "./Nav";
 function App() {
   return (
     <div className="app">
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX ORIGINALS"
--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 const base_url = "https://image.tmdb.org/t/p/original/";
+const API_KEY = "9860402eeec361255544095ad0dc7620";
 function Row({ title, url, isLarge }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
   useEffect(() => {
     async function fetchMovies() {
       const response = await axios.get(url);
@@ -11,22 +15,52 @@ function Row({ title, url, isLarge }) {
     }
     fetchMovies();
   }, [url]);
-
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const Handler = (movie) => {
+    console.log(movie?.name);
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(
+        movie?.name ||
+          movie?.original_name ||
+          movie?.title ||
+          movie?.original_title ||
+          ""
+      )
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className="row_posters">
-        {movies.map((movie) => (
-          <img
-            key={movie.id}
-            className={`row_poster ${isLarge && "row_poster_large"}`}
-            src={`${base_url}${
-              isLarge ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-          />
-        ))}
+        {movies.map((movie) => {
+          console.count(movie);
+          return (
+            <img
+              onClick={() => Handler(movie)}
+              key={movie.id}
+              className={`row_poster ${isLarge && "row_poster_large"}`}
+              src={`${base_url}${
+                isLarge ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          );
+        })}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
